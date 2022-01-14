b463f0c3c1ac224d089fe51605cbd888d23550e4
cardano-on-the-road <cardano.on.the.road@gmail.com>
2022-01-14 21:07:48 +0000

    stock screener updated

--- stock-screener/stocks_report.py
+++ stock-screener/stocks_report.py
@@ -42,7 +42,7 @@ class stocks_report:
         return report
     
     def add_fundamental_data(self, df):
-        ratios = ['trailingPE','marketCap', 'forwardEps', 'trailingEps', 'bookValue', 'priceToBook', 'pegRatio', 'sector', 'industry']
+        ratios = ['trailingPE','marketCap', 'forwardEps', 'trailingEps', 'bookValue', 'priceToBook', 'pegRatio', 'sector', 'industry', 'targetLowPrice','targetMeanPrice','targetHighPrice','recommendationMean', 'recommendationKey']
         new_df = df.reindex(columns = df.columns.tolist() + ratios)
         for row_index in new_df.index:
             try:
